85b3949f33bfebcb83960044ee3ea0d24249e3ab
Ivan Folbort <petrarce@gmail.com>
2019-11-01 21:07:49 +0100

    revert learnSPH_app to initiali state

--- app/main.cpp
+++ app/main.cpp
@@ -5,101 +5,63 @@
 #include <algorithm>    // std::max
 
 #include <Eigen/Dense>
-#include <data_set.h>
-#include <types.hpp>
-#include <particle_sampler.h>
-#include <CompactNSearch>
 
 #include <vtk_writer.h>
-#include <solver.h>
-#include <chrono>
-using namespace CompactNSearch;
 
-
-int main(int argc, char** argv)
+int main()
 {
 	std::cout << "Welcome to the learnSPH framework!!" << std::endl;
 	std::cout << "Generating a sample scene...";
 
-	assert(argc == 10);
-	Vector3R upper_corner = {stod(argv[1]), stod(argv[2]), stod(argv[3])};
-	Vector3R lover_corner = {stod(argv[4]), stod(argv[5]), stod(argv[6])};
-	Real sampling_distance = stod(argv[7]);
-	double compactSupportFactor = stod(argv[8]);
-	int genWithBorder = stoi(argv[9]) * compactSupportFactor;
-	NeighborhoodSearch ns(sampling_distance*compactSupportFactor);
 
+	// Generate particles
+	std::vector<Eigen::Vector3d> particles;
+	std::vector<Eigen::Vector3d> velocities;
 
-	//generate fluid particle cube
-	auto t0 = std::chrono::high_resolution_clock::now();
-	NormalPartDataSet* fluidParticles = 
-		static_cast<NormalPartDataSet*>(learnSPH::ParticleSampler::sample_normal_particles(upper_corner, 
-															lover_corner, 
-															1000, 
-															sampling_distance));
-	auto t1 = std::chrono::high_resolution_clock::now();
-	printf("fluid particle sampling time: %ld ms\n", 
-		std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count());
-
-	auto fluidPartilesPset = ns.add_point_set((Real*)(fluidParticles->getParticlePositions().data()),
-						fluidParticles->getNumberOfParticles(),
-						true,
-						true, 
-						true);
-	std::cout<< "number of fluid particles: " << fluidParticles->getNumberOfParticles() << endl;
-
-	//generate border particle box if specified
-	upper_corner = upper_corner+
-					(upper_corner - lover_corner).normalized()*(sampling_distance + genWithBorder);
-	lover_corner = lover_corner + 
-					(lover_corner - upper_corner).normalized()*(sampling_distance + genWithBorder);
-	t0 = std::chrono::high_resolution_clock::now();
-	BorderPartDataSet* borderParticles = 
-		static_cast<BorderPartDataSet*>(learnSPH::ParticleSampler::sample_border_box(upper_corner, 
-															lover_corner, 
-															1000, 
-															sampling_distance));
-	t1 = std::chrono::high_resolution_clock::now();
-	printf("border particle sampling time: %ld ms\n", 
-		std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count());
-
-	auto borderParticlesPset = ns.add_point_set((Real*)borderParticles->getParticlePositionsData(),
-						borderParticles->getNumberOfParticles(),
-						false,
-						true, 
-						true);
-	std::cout<< "number of border particles: " << borderParticles->getNumberOfParticles() << endl;
-
-
-	ns.update_point_sets();
-
-	vector<vector<vector<unsigned int>>> particleNeighbors;
-	particleNeighbors.resize(fluidParticles->getNumberOfParticles());
-	for(int i = 0; i < fluidParticles->getNumberOfParticles(); i++){
-		ns.find_neighbors(fluidPartilesPset, i, particleNeighbors[i]);
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 20; j++) {
+			for (int k = 0; k < 20; k++) {
+				double x = static_cast<double> (rand()) / static_cast <float> (RAND_MAX);
+				double y = static_cast<double> (rand()) / static_cast <float> (RAND_MAX);
+				double z = static_cast<double> (rand()) / static_cast <float> (RAND_MAX);
+				particles.push_back(Eigen::Vector3d(x, y, z));
+				velocities.push_back(Eigen::Vector3d(0.0, 0.0, 0.0));
+			}
+		}
 	}
 
-	t0 = std::chrono::high_resolution_clock::now();
-	learnSPH::Solver::calculate_dencities(*fluidParticles, *borderParticles, particleNeighbors);
-	t1 = std::chrono::high_resolution_clock::now();
-	printf("density calcultaions time: %ld ms\n", 
-		std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count());
-	// Generate particles
-	std::string filename = "../res/fluid_particle_data_set.vtk";
-	learnSPH::saveParticlesToVTK(filename, 
-									fluidParticles->getParticlePositions(), 
-									fluidParticles->getParticleDencities(), 
-									fluidParticles->getParticleVelocities());
+	// Initialize data vectors
+	std::vector<double> particles_scalar_data(particles.size());
+	std::vector<Eigen::Vector3d> particles_vector_data(particles.size());
+	
+	// Simulation loop
+	for (int time_step = 0; time_step < 100; time_step++) {
 
-	filename = "../res/boreder_particle_data_set.vtk";
-	learnSPH::saveParticlesToVTK(filename, 
-									borderParticles->getParticlePositions(), 
-									borderParticles->getParticleVolume(), 
-									borderParticles->getParticlePositions());
+		std::cout << "processing frame: " << time_step << std::endl;
 
- 
-	delete fluidParticles;
-	delete borderParticles;
+		for (int particle_i = 0; particle_i < (int)particles.size(); particle_i++) {
+			// Move particles a bit down in the Z direction
+			velocities[particle_i][2] -= 0.01;
+			particles[particle_i][2] += velocities[particle_i][2];
+
+			// Clamp the motion to the floor
+			if (particles[particle_i][2] <= 0.0) {
+				velocities[particle_i][2] *= -0.6;
+				particles[particle_i][2] = 0.0;
+			}
+			
+			// Update scalar data
+			particles_scalar_data[particle_i] = particles[particle_i].norm();
+
+			// Update vector data
+			particles_vector_data[particle_i] = (Eigen::Vector3d(0.5, 0.5, 1.0) - particles[particle_i]).normalized();
+		}
+
+		// Save output
+		const std::string filename = "../res/example_" + std::to_string(time_step) + ".vtk";
+		learnSPH::saveParticlesToVTK(filename, particles, particles_scalar_data, particles_vector_data);
+	}
+
 	std::cout << "completed!" << std::endl;
 	std::cout << "The scene files have been saved in the folder `<build_folder>/res`. You can visualize them with Paraview." << std::endl;
 
